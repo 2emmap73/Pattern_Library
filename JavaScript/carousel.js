1a21b4b1b6e0f30b95d15a3153589e14791804c9
2emmap73 <2emmap73@solent.ac.uk>
2018-05-05 23:29:44 +0100

    Added comments.
    Added Comments where appropriate.

--- JavaScript/carousel.js
+++ JavaScript/carousel.js
@@ -2,17 +2,17 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += n); // This code enables the user to move back and forth between images, i.e. Next and Previous.
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slideIndex = n);  // This is for thumbnail image controls.
 }
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("slides");
-  var cdt = document.getElementsByClassName("cdt");
+  var slides = document.getElementsByClassName("slides"); // This code finds the class name 'slides' in the HTML code of the Carousel.html.
+  var cdt = document.getElementsByClassName("cdt");  // This code finds the span class 'cdt' in the HTML code. It displays the images dots.
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
